7cde90597c389c07aa629695dbc80e5eb7fc9330
Anirban Banerjee <65713021+Anirban007-jpg@users.noreply.github.com>
2024-02-09 13:55:28 +0530

    Update server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ app.use(function (err, req, res, next) {
 
 // handle port
 const port = process.env.PORT || 8000;
-app.listen(port, '0.0.0.0' , function(err) => {
+app.listen(port, '0.0.0.0' , function(err) {
     console.log(`Server is running }`)
 })
 
